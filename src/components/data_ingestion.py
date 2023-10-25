89e80237b3da64b710aebf3a52156cf1cc461acf
adityavarma99 <adityavarma759@gmail.com>
2023-10-25 16:45:25 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModeTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -23,8 +26,8 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data Ingestion method or component")
         try:
-            df=pd.read_csv('notebook/data/stud.csv')      ## read the dataset , we are reading it from local fldr, we can do it from mangodb also
-            logging.info('Read the dataset as dataframe')                               # logging the datet
+            df=pd.read_csv('notebook/data/stud.csv')      ## read the dataset ,we are reading it from local folder, we can do it from mangodb also
+            logging.info('Read the dataset as dataframe')                               # logging the data
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
@@ -54,7 +57,11 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModeTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))    # gives r2 score
+
 
 
             
