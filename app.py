d0e5183578d0ef6574722ac8cc613f602e43ad66
adityavarma99 <adityavarma759@gmail.com>
2023-10-29 23:31:11 +0530

    Prediction pipeline

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
+application=Flask(__name__)
+
+app=application
+
+## Route for a home page
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)     # when we call predict it will go to predit_pipene,py to get predict function to transform, predict and get the predicted data
+        print("Before Prediction")
+
+        predict_pipeline=PredictPipeline()   # we created result as prediction in home.html
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+    
+
+if __name__=="__main__":
+    app.run(host="127.0.0.1",debug=True)        
+
+                  
+  
+    
+
--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ import pandas as pd
 
 from src.exception import CustomException
 
+import pickle
+
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -17,7 +19,7 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e,sys)
@@ -49,3 +51,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
